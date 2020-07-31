d46e766b4514cc0a3e9747c2b7f11a301965c476
NoWifi4U <max.shamber@gmail.com>
2020-07-31 22:42:15 +0300

    Add namespace ref

--- nowifi/math/math.hpp
+++ nowifi/math/math.hpp
@@ -28,6 +28,34 @@ namespace nw {
             return (val1 - val2) > val1;
         }
 
+        namespace ref {
+        
+            template <class Ty>
+            constexpr inline const Ty& min(const Ty& val1, const Ty& val2)
+            {
+                return val1 < val2 ? val1 : val2;
+            }
+
+            template <class Ty>
+            constexpr inline const Ty& max(const Ty& val1, const Ty& val2)
+            {
+                return val1 < val2 ? val2 : val1;
+            }
+
+            template <class Ty1, class Ty2>
+            constexpr inline bool willOverflow(const Ty1& val1, const Ty2& val2)
+            {
+                return (val1 + val2) < val1;
+            }
+
+            template <class Ty1, class Ty2>
+            constexpr inline bool willUnderflow(const Ty1& val1, const Ty2& val2)
+            {
+                return (val1 - val2) > val1;
+            }
+        
+        }
+
     } // namespace Math
 
 } // namespace nw
