9086887402e6f4df9488212f7fc5cff65f7bfa92
NoWifi4U <max.shamber@gmail.com>
2020-08-01 04:16:10 +0300

    Update _CONSTEXPR17

--- nowifi/math/math.hpp
+++ nowifi/math/math.hpp
@@ -5,25 +5,25 @@ namespace nw {
     namespace Math {
 
         template <class Ty>
-        constexpr inline const Ty min(const Ty val1, const Ty val2)
+        _CONSTEXPR17 inline const Ty min(const Ty val1, const Ty val2)
         {
             return val1 < val2 ? val1 : val2;
         }
 
         template <class Ty>
-        constexpr inline const Ty max(const Ty val1, const Ty val2)
+        _CONSTEXPR17 inline const Ty max(const Ty val1, const Ty val2)
         {
             return val1 < val2 ? val2 : val1;
         }
 
         template <class Ty1, class Ty2>
-        constexpr inline bool willOverflow(const Ty1 val1, const Ty2 val2)
+        _CONSTEXPR17 inline bool willOverflow(const Ty1 val1, const Ty2 val2)
         {
             return (val1 + val2) < val1;
         }
 
         template <class Ty1, class Ty2>
-        constexpr inline bool willUnderflow(const Ty1 val1, const Ty2 val2)
+        _CONSTEXPR17 inline bool willUnderflow(const Ty1 val1, const Ty2 val2)
         {
             return (val1 - val2) > val1;
         }
@@ -31,25 +31,25 @@ namespace nw {
         namespace ref {
         
             template <class Ty>
-            constexpr inline const Ty& min(const Ty& val1, const Ty& val2)
+            inline const Ty& min(const Ty& val1, const Ty& val2)
             {
                 return val1 < val2 ? val1 : val2;
             }
 
             template <class Ty>
-            constexpr inline const Ty& max(const Ty& val1, const Ty& val2)
+            inline const Ty& max(const Ty& val1, const Ty& val2)
             {
                 return val1 < val2 ? val2 : val1;
             }
 
             template <class Ty1, class Ty2>
-            constexpr inline bool willOverflow(const Ty1& val1, const Ty2& val2)
+            inline bool willOverflow(const Ty1& val1, const Ty2& val2)
             {
                 return (val1 + val2) < val1;
             }
 
             template <class Ty1, class Ty2>
-            constexpr inline bool willUnderflow(const Ty1& val1, const Ty2& val2)
+            inline bool willUnderflow(const Ty1& val1, const Ty2& val2)
             {
                 return (val1 - val2) > val1;
             }
