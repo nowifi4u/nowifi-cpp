96e77352523bcfb5b2d880ae1646b4f6973b3a95
NoWifi4U <max.shamber@gmail.com>
2020-08-01 04:16:03 +0300

    Update _CONSTEXPR17

--- nowifi/math/map.hpp
+++ nowifi/math/map.hpp
@@ -16,27 +16,27 @@ namespace nw {
 			return Map::map(input, input_low, input_high, output_low, output_high);
 		}
 
-		static constexpr inline Ty map_scale(const Ty input, const Ty input_low, const Ty input_high) {
+		static _CONSTEXPR17 inline Ty map_scale(const Ty input, const Ty input_low, const Ty input_high) {
 			const Ty input_size = input_high - input_low;
 			const Ty input_delta = input - input_low;
 
 			return input_delta / input_size;
 		}
 
-		static constexpr inline Ty map_unscale(const Ty scale, const Ty output_low, const Ty output_high) {
+		static _CONSTEXPR17 inline Ty map_unscale(const Ty scale, const Ty output_low, const Ty output_high) {
 			const Ty output_size = output_high - output_low;
 			const Ty output_delta = output_size * scale;
 
 			return output_low + output_delta;
 		}
 
-		static constexpr inline Ty map(const Ty input, const Ty input_low, const Ty input_high, const Ty output_low, const Ty output_high) {
+		static _CONSTEXPR17 inline Ty map(const Ty input, const Ty input_low, const Ty input_high, const Ty output_low, const Ty output_high) {
 			const Ty scale = map_scale(input, input_low, input_high);
 
 			return map_unscale(scale, output_low, output_high);
 		}
 
-		static constexpr inline Ty map_reverse(const Ty input, const Ty low, const Ty high) {
+		static _CONSTEXPR17 inline Ty map_reverse(const Ty input, const Ty low, const Ty high) {
 			const Ty delta = input - low;
 
 			return high - delta;
