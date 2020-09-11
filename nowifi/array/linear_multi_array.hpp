5163261e7072a15808aad52f16f590baf5b4d81a
Maxim Shamber <max.shamber@gmail.com>
2020-09-11 23:50:36 +0300

    Update

--- nowifi/array/linear_multi_array.hpp
+++ nowifi/array/linear_multi_array.hpp
@@ -42,50 +42,108 @@ namespace nw {
 		template <class Ty>
 		using const_iterator = const iterator<Ty>;
 
-		static inline index_type calcMultipliers(const index_type& size)
+		_NODISCARD inline
+			static index_type calcMultipliers(const index_type& size)
 		{
 			index_type result;
 			size_t curr = 1;
 			for_loop::raw_i<Dim>::call([&size, &result, &curr](size_t idx)
-			{
-				curr *= size[idx];
-				result[idx] = curr;
-			});
+				{
+					curr *= size[idx];
+					result[idx] = curr;
+				});
 			return result;
+		}
+
+	protected:
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t _layerOffset(const index_type_any<HiDim>& size, size_t pos, size_t offset)
+		{
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
+
+			return (offset + pos) * size[HiDim - Dim + 1];
+		}
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t _layerOffset(const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos, size_t offset)
+		{
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
+
+			return (offset + pos[HiDim - Dim]) * size[HiDim - Dim + 1];
+		}
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t _impl_calcOffset(const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos, size_t offset = 0)
+		{
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
+
+			return below_type::_impl_calcOffset(size, pos, _layerOffset(size, pos, offset));
+		}
+
+	public:
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t calcOffset(const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos)
+		{
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
+
+			return _impl_calcOffset(size, pos);
 		}
 
 		//-------------------- Memory managing operations --------------------//
 
 		template <class Ty> _NODISCARD inline
-		static iterator<Ty> allocate(const index_type& multipliers)
+			static iterator<Ty> allocate(const index_type& size)
 		{
-			return new Ty[multipliers[0]];
+			return new Ty[calcMultipliers(size)[0]];
 		}
 
 		template <class Ty> inline
-		static void deallocate(iterator<Ty> arr)
+			static void deallocate(iterator<Ty> arr)
 		{
 			delete[] arr;
 		}
 
 		template <class Ty, size_t HiDim> _NODISCARD inline
-		static Ty& get(Ty* arr, const index_type& multipliers, const index_type_any<HiDim>& pos)
+			static Ty& get(Ty* arr, const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos)
 		{
-			static_assert(HiDim >= Dim, "Parameter <pos> too small");
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
 
-			return below_type::template get<Ty>(arr + (multipliers[HiDim - Dim] * pos[HiDim - Dim]), multipliers, pos);
+			return arr[calcOffset(size, pos)];
 		}
 
 		template <class Ty, size_t HiDim> _NODISCARD inline
-		static const Ty& get(const Ty* arr, const index_type& multipliers, const index_type_any<HiDim>& pos)
+			static const Ty& get(const Ty* arr, const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos)
 		{
-			static_assert(HiDim >= Dim, "Parameter <pos> too small");
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
 
-			return below_type::template get<Ty>(arr + (multipliers[HiDim - Dim + 1] * pos[HiDim - Dim]), multipliers, pos);
+			return arr[calcOffset(size, pos)];
 		}
 
 		//-------------------- Non-modifying sequence operations --------------------//
 
+	protected:
+
+		template <class Ty, size_t HiDim> _NODISCARD inline
+			static bool _impl_all(const Ty* arr, const index_type_any<HiDim>& size, const Ty& val, size_t offset = 0)
+		{
+			static_assert(HiDim >= Dim, "Parameter <size> too small");
+
+			for (int idx = 0; idx < size[HiDim - Dim]; idx++)
+			{
+				if (!below_type::template _impl_all<Ty>(arr, size, _layerOffset(size, idx, offset))) return false;
+			}
+			return true;
+		}
+
+	public:
+
+		template <class Ty, size_t HiDim> _NODISCARD inline
+			static bool all(const Ty* arr, const index_type_any<HiDim>& size, const Ty& val)
+		{
+			return _impl_all(arr, size, val);
+		}
 
 	};
 
@@ -119,11 +177,46 @@ namespace nw {
 		template <class Ty>
 		using iterator = Ty*;
 
-		static inline index_type calcMultipliers(const index_type& size)
+		_NODISCARD inline
+			static index_type calcMultipliers(const index_type& size)
 		{
 			index_type result;
 			result[0] = size[0];
 			return result;
+		}
+
+	protected:
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t _layerOffset(const index_type_any<HiDim>& size, size_t pos, size_t offset)
+		{
+			static_assert(HiDim >= 1, "Parameter <size> too small");
+
+			return offset + pos;
+		}
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t _layerOffset(const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos, size_t offset)
+		{
+			static_assert(HiDim >= 1, "Parameter <size> too small");
+
+			return offset + pos[HiDim - 1];
+		}
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t _impl_calcOffset(const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos, size_t offset = 0)
+		{
+			static_assert(HiDim >= 1, "Parameter <size> too small");
+
+			return offset + pos[HiDim - 1];
+		}
+
+	public:
+
+		template <size_t HiDim> _NODISCARD inline
+			static size_t calcOffset(const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos)
+		{
+			return _impl_calcOffset(size, pos);
 		}
 
 		//-------------------- Memory managing operations --------------------//
@@ -141,22 +234,45 @@ namespace nw {
 		}
 
 		template <class Ty, size_t HiDim> _NODISCARD inline
-		static Ty& get(Ty* arr, const index_type& multipliers, const index_type_any<HiDim>& pos)
+			static Ty& get(Ty* arr, const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos)
 		{
-			static_assert(HiDim >= 1, "Parameter <pos> too small");
+			static_assert(HiDim >= 1, "Parameter <size> too small");
 
-			return *(arr + pos[HiDim - 1]);
+			return arr[calcOffset(size, pos)];
 		}
 
 		template <class Ty, size_t HiDim> _NODISCARD inline
-		static const Ty& get(const Ty* arr, const index_type& multipliers, const index_type_any<HiDim>& pos)
+			static const Ty& get(const Ty* arr, const index_type_any<HiDim>& size, const index_type_any<HiDim>& pos)
 		{
-			static_assert(HiDim >= 1, "Parameter <pos> too small");
+			static_assert(HiDim >= 1, "Parameter <size> too small");
 
-			return *(arr + pos[HiDim - 1]);
+			return arr[calcOffset(size, pos)];
 		}
 
 		//-------------------- Non-modifying sequence operations --------------------//
+
+	protected:
+
+		template <class Ty, size_t HiDim> _NODISCARD inline
+			static bool _impl_all(const Ty* arr, const index_type_any<HiDim>& size, const Ty& val, size_t offset = 0)
+		{
+			static_assert(HiDim >= 1, "Parameter <size> too small");
+
+			for (int idx = 0; idx < size[HiDim - 1]; idx++)
+			{
+				if (arr[offset + idx] != val) return false;
+			}
+			return true;
+		}
+
+	public:
+
+		template <class Ty, size_t HiDim> _NODISCARD inline
+			static bool all(const Ty* arr, const index_type_any<HiDim>& size, const Ty& val)
+		{
+			return _impl_all(arr, size, val);
+		}
+
 	};
 
 	////////////////////////////////                       ////////////////////////////////
